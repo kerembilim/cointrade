f05f29cd377c920b799054804ab8821b8aa7241b
Ahmet Kerem Bilim <ahmetkerem.bilim@ibtech.com.tr>
2022-01-30 00:22:45 +0300

    [ADD] Added admin panel.

--- coin-trade-client/src/app/app-routing.module.ts
+++ coin-trade-client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LandingComponent } from './landing/landing.component';
 
 
-const routes: Routes = [{ path: '', component: LandingComponent }];
+const routes: Routes = [{ path: '', component: LandingComponent },
+{ path: 'dashboard', component: DashboardComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- coin-trade-client/src/app/app.module.ts
+++ coin-trade-client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DashboardModule } from './dashboard/dashboard.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { LandingComponent } from './landing/landing.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
